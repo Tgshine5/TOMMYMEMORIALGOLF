ea18310094b004efafbb130b4eb427af3bc66e50
Tgshine5 <tgth7756@gmail.com>
2024-12-04 14:56:46 -0500

    Add files via upload

---
+++ eleventy.config.js
@@ -0,0 +1,5 @@
+export default function (eleventyConfig){
+    eleventyConfig.addPassthroughCopy("Styles/");
+    eleventyConfig.addPassthroughCopy("images/");
+    eleventyConfig.addPassthroughCopy("Scripts/");}
+    
